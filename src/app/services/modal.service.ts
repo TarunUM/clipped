b31ec06ccad7fad28f8f7de0dc8ff1c592e2f8dc
TarunM <tarun.8792@gmail.com>
2023-04-09 18:54:45 +0530

    Completed Component Design

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ModalService {
-  visible = false;
+  private modals: IModal[] = [];
 
-  constructor() { }
+  constructor() {}
+
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false,
+    });
+  }
+
+  unregister(id: string) {
+    this.modals = this.modals.filter((modal) => modal.id !== id);
+  }
+
+  isModalOpen(id: string): boolean {
+    // return !!this.modals.find((modal) => modal.id === id)?.visible;
+    return Boolean(this.modals.find((modal) => modal.id === id)?.visible);
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.find((modal) => modal.id === id);
+    if (modal) {
+      modal.visible = !modal.visible;
+    }
+  }
 }
