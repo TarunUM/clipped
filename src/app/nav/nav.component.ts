c58c607e4347cf8fef359d1a9ede541554448846
TarunUM <tarun.8792@gmail.com>
2023-05-28 16:13:55 +0530

    Parameters Routing

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -11,10 +10,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 export class NavComponent {
   isAuthenticated = false;
 
-  constructor(
-    public modal: ModalService,
-    public auth: AuthService,
-  ) {}
+  constructor(public modal: ModalService, public auth: AuthService) {}
 
   openModal($event: Event) {
     $event.preventDefault();
