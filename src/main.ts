f50ab2d6024d0212e66c20371ea5c57390dcd9a9
TarunUM <tarun.8792@gmail.com>
2023-07-05 14:18:49 +0530

    final setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { environment } from './environments/environment.development';
+import { environment } from './environments/environment';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 import { AppModule } from './app/app.module';
 
 firebase.initializeApp(environment.firebase);
+
 let appInit = false;
+
 firebase.auth().onAuthStateChanged(() => {
   if (!appInit) {
     platformBrowserDynamic()
